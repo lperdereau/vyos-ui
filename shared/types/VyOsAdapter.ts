e306232e98037fd0827af1750039788b0eab3609
Louis PERDEREAU <leolouis.perdereau@gmail.com>
2025-03-09 19:36:15 +0100

    fix: Patch GraphQLVyOS.getLayout

--- shared/types/VyOsAdapter.ts
+++ shared/types/VyOsAdapter.ts
@@ -1,10 +1,7 @@
-import {string} from "postcss-selector-parser";
-import type {Hardware} from "#shared/types/hardware";
-import type {Layout} from "#shared/types/layout";
-
-
+import type { Hardware } from "#shared/types/hardware";
+import type { Layout } from "#shared/types/layout";
 
 export interface VyOsAdapter {
-    getDashboard() : Promise<Hardware>;
-    getLayout(): Promise<Layout>
+  getDashboard(): Promise<Hardware>;
+  getLayout(): Promise<Layout>;
 }

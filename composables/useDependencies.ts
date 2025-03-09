8539ffaba8a54e21b2039fbebf84dcad39dd6e63
Louis PERDEREAU <leolouis.perdereau@gmail.com>
2025-03-09 19:34:18 +0100

    fix: Patch GraphQLVyOS.getLayout

--- composables/useDependencies.ts
+++ composables/useDependencies.ts
@@ -1,35 +1,37 @@
-import {GraphQLClient} from "graphql-request";
-import {useGraphQL} from "~/composables/useGraphQL";
-import type {VyOsAdapter} from "#shared/types/VyOsAdapter";
-import type {Hardware} from "#shared/types/hardware";
-import {computeHardware} from "#shared/types/hardware";
-import {dashboard} from "#shared/graphql/dashboard";
-import {InMemoryVyOs} from "#shared/types/inMemoryVyOs";
-import type {Layout} from "#shared/types/layout";
-import {layout} from "#shared/graphql/layout";
+import { GraphQLClient } from "graphql-request";
+import { useGraphQL } from "~/composables/useGraphQL";
+import type { VyOsAdapter } from "#shared/types/VyOsAdapter";
+import type { Hardware } from "#shared/types/hardware";
+import { computeHardware } from "#shared/types/hardware";
+import { dashboard } from "#shared/graphql/dashboard";
+import { InMemoryVyOs } from "#shared/types/inMemoryVyOs";
+import type { Layout } from "#shared/types/layout";
+import { layout } from "#shared/graphql/layout";
 
 class GraphQLVyOs implements VyOsAdapter {
-    constructor(private readonly client: GraphQLClient) {
-        this.client = client;
-    }
+  constructor(private readonly client: GraphQLClient) {
+    this.client = client;
+  }
 
-    async getDashboard(): Promise<Hardware> {
-        const data = await this.client.request(dashboard, { key: "foo" });
-        return computeHardware(data);
+  async getDashboard(): Promise<Hardware> {
+    const data = await this.client.request(dashboard, { key: "foo" });
+    return computeHardware(data);
+  }
 
-    }
-
-    async getLayout(): Promise<Layout> {
-        const data = await this.client.request(layout, { key: "foo" });
-        return {host_name: data.result.host_name, version: {version: data.result.version}};
-    }
+  async getLayout(): Promise<Layout> {
+    const data = await this.client.request(layout, { key: "foo" });
+    return {
+      host_name: data.SystemStatus.data.result.host_name,
+      version: { version: data.SystemStatus.data.result.version.version },
+    };
+  }
 }
 
-export const useDependencies = (): {vyOsAdapter: VyOsAdapter } => {
-    //TODO Changer la var d'env
-    if (process.env.NODE_ENV === "development") {
-        return {vyOsAdapter: new InMemoryVyOs()};
-    }
-    const {client} = useGraphQL();
-    return {vyOsAdapter: new GraphQLVyOs(client)};
+export const useDependencies = (): { vyOsAdapter: VyOsAdapter } => {
+  //TODO Changer la var d'env
+  if (process.env.NODE_ENV === "development") {
+    return { vyOsAdapter: new InMemoryVyOs() };
+  }
+  const { client } = useGraphQL();
+  return { vyOsAdapter: new GraphQLVyOs(client) };
 };

096ca6fe46bc3a228bdf6ef07489f764eb3ad8e9
Louis PERDEREAU <leolouis.perdereau@gmail.com>
2025-03-10 21:23:10 +0100

    feat: set to localhost endpoint

--- composables/useGraphQL.ts
+++ composables/useGraphQL.ts
@@ -1,6 +1,6 @@
 import { GraphQLClient } from "graphql-request";
 
-const endpoint = process.env.ENDPOINT || "https://10.14.99.190/graphql"; // Example API
+const endpoint = process.env.ENDPOINT || "https://localhost/graphql"; // Example API
 
 export const useGraphQL = () => {
   const client = new GraphQLClient(endpoint);

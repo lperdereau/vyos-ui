8d2c711d1b7af036546b0e97231ace469e745e2d
Jeremy Chauvin <jeremychauvin10@gmail.com>
2025-03-22 16:32:04 +0100

    test: add test on dashboard query and converter

--- gateways/__test__/graphQLVyOs.spec.ts
+++ gateways/__test__/graphQLVyOs.spec.ts
@@ -2,22 +2,71 @@ import { afterAll, afterEach, beforeAll, describe, expect, it } from 'vitest'
 import { GraphQLClient } from 'graphql-request'
 import { GraphQLVyOs } from '~/gateways/graphQLVyOs'
 import { server } from '~/gateways/__test__/mocks'
+import type { CPU, RAM, Storage } from '#shared/types/hardware'
 
+const fakeCpu = (): CPU => ({
+  load: 0,
+  loadAvg15Minutes: 0,
+  loadAvg1Minute: 0,
+  loadAvg5Minutes: 0,
+  type: 'QEMU Virtual CPU version 2.5+',
+})
+
+const fakeStorage = (): Storage => {
+  return {
+    total: {
+      size: 9.50,
+      unit: 'GiB',
+    },
+    used: {
+      size: 597.00,
+      unit: 'MiB',
+    },
+  }
+}
+const fakeRam = (): RAM => {
+  return {
+    total: {
+      size: 1.94,
+      unit: 'GiB',
+    },
+    used: {
+      size: 503.97,
+      unit: 'MiB',
+    },
+  }
+}
 describe('graphQLVyOS Adapter', () => {
   beforeAll(() => {
     server.listen()
   })
+
   afterEach(() => {
     server.resetHandlers()
   })
+
   afterAll(() => {
     server.close()
   })
-  it('should return the layout', async () => {
-    const graphQLVyOs = new GraphQLVyOs(new GraphQLClient('http://localhost:3000'))
-    expect(await graphQLVyOs.getLayout()).toStrictEqual({
-      host_name: 'vyos-lperdereau-1',
-      version: { version: '1.5-rolling-202501200007' },
+
+  describe('Layout', () => {
+    it('should return the layout', async () => {
+      const graphQLVyOs = new GraphQLVyOs(new GraphQLClient('http://localhost:3000'))
+      expect(await graphQLVyOs.getLayout()).toStrictEqual({
+        host_name: 'vyos-lperdereau-1',
+        version: { version: '1.5-rolling-202501200007' },
+      })
+    })
+
+    describe('Dashboard', () => {
+      it('should return the dashboard', async () => {
+        const graphQLVyOs = new GraphQLVyOs(new GraphQLClient('http://localhost:3000'))
+        const dashboad = await graphQLVyOs.getDashboard()
+
+        expect(dashboad.cpu).toStrictEqual(fakeCpu())
+        expect(dashboad.storage).toStrictEqual(fakeStorage())
+        expect(dashboad.ram).toStrictEqual(fakeRam())
+      })
     })
   })
 })

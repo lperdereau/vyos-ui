ddad976e64992e5200dd7d5232ed6ab0d58422bc
Louis PERDEREAU <leolouis.perdereau@gmail.com>
2025-03-30 23:46:22 +0200

    WIP

---
+++ components/Interface/Edge.vue
@@ -0,0 +1,21 @@
+<template>
+  <BezierEdge
+    :source-x="sourceX"
+    :source-y="sourceY"
+    :target-x="targetX"
+    :target-y="targetY"
+    :source-position="sourcePosition"
+    :target-position="targetPosition"
+  />
+</template>
+
+<script setup lang="ts">
+import type { EdgeProps } from '@vue-flow/core'
+import { BezierEdge } from '@vue-flow/core'
+
+type EdgeData = {
+  active: boolean
+}
+
+defineProps<EdgeProps<EdgeData>>()
+</script>
